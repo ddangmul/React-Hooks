b6d9e64d3e1bf75fa77aaf00c4321c615e4ef75a
ddangmul <j.ddangmul@gmail.com>
2024-12-23 13:46:08 +0900

    커스텀 훅 useFetch 생성 & 상태 관리 & 상태값 반환

---
+++ src/hooks/useFecth.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+// 리액트에서 use로 시작하는 함수는 훅으로 간주되고, 특정 규칙을 부여받음
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn(); // 경우에 맞는 fetch함수 불러와서 사용
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]); // 의존성으로 fetch함수 입력
+
+  return {
+    isFetching,
+    error,
+    fetchedData,
+  };
+}
